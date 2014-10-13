92f144909f6bd99585b14795ddff53166e80e599
Garrett Bartley <wgbartley@gmail.com>
2014-10-13 18:05:59 -0400

    added missing ports

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo docker run -t -d --name=Graphite -h graphite \
-	    -p 8000:8000 \
+	    -p 8000:8000 -p 2003:2003 -p 8125:8125/udp \
 	    -v $(pwd)/data:/storage \
 	    odroid_u3/graphite_statsd
